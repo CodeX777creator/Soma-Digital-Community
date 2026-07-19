3783bc1ba29792712361340889d7948ddd4637dc
SOMALeoAfrica <somaleoafrica@gmail.com>
2026-07-20 01:22:28 +0300

    rewind

--- src/social/oauth.ts
+++ src/social/oauth.ts
@@ -68,9 +68,9 @@ const SOCIAL_OAUTH_PROVIDER_RULES: SocialOAuthProviderRule[] = [
   },
   {
     providerId: 'instagram',
-    flowMode: 'oauth2',
-    supportsRefreshToken: false,
-    requiresPkce: false,
+    flowMode: 'pkce-oauth2',
+    supportsRefreshToken: true,
+    requiresPkce: true,
     callbackPath: '/api/social/oauth/instagram/callback',
     defaultScopes: ['instagram_basic', 'instagram_content_publish'],
     authorizationUrl: process.env.SOCIAL_OAUTH_AUTH_URL_INSTAGRAM || null,
